69ad4f3969af41dd62a3d2ab7c6e318f8ab92ca9
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2025-02-09 22:50:41 -0300

    incializa as configurações gpio dos pinos dos leds e botões

--- main.c
+++ main.c
@@ -1,14 +1,50 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#define PIN_LED_RED 13   // Pino do LED vermelho
+#define PIN_LED_GREEN 11 // Pino do LED verde
+#define PIN_LED_BLUE 12  // Pino do LED azul
+#define TIME_SLEEP 100   // Tempo de espera em milissegundos para o led piscar 5 vezes por segundo, 1000/5 = 200ms e um ciclo leva 100ms*2 = 200ms
+#define PIN_MATRIZ_LED 7 // Pino da matriz de LEDs
+#define PIN_BUTTON_A 5   // Pino do botão A
+#define PIN_BUTTON_B 6   // Pino do botão B
 
+void init_gpio_settings()
+{
+    // Inicialização do pino vermelho
+    gpio_init(PIN_LED_RED);
+    gpio_set_dir(PIN_LED_RED, GPIO_OUT);
+
+    // Inicialização do pino verde
+    gpio_init(PIN_LED_GREEN);
+    gpio_set_dir(PIN_LED_GREEN, GPIO_OUT);
+
+    // Inicialização do pino azul
+    gpio_init(PIN_LED_BLUE);
+    gpio_set_dir(PIN_LED_BLUE, GPIO_OUT);
+
+    // Configuração do botão A
+    gpio_init(PIN_BUTTON_A);
+    gpio_set_dir(PIN_BUTTON_A, GPIO_IN);
+    gpio_pull_up(PIN_BUTTON_A);
+
+    // Configuração do botão B
+    gpio_init(PIN_BUTTON_B);
+    gpio_set_dir(PIN_BUTTON_B, GPIO_IN);
+    gpio_pull_up(PIN_BUTTON_B);
+}
 
 int main()
 {
+    // Inicialização da comunicação serial
     stdio_init_all();
 
-    while (true) {
+    // Inicialização das configurações dos pinos
+    init_gpio_settings();
+
+    while (true)
+    {
         printf("Hello, world!\n");
-        sleep_ms(1000);
+        sleep_ms(TIME_SLEEP);
     }
 }
